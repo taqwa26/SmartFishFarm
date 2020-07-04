5b7c38f06855fbae148eb9603072f487fb331cc9
taqwa26 <taqwaalmutawakkil97@gmail.com>
2020-07-05 00:56:20 +0800

    v1.0

---
+++ aFiFa2.py
@@ -0,0 +1,177 @@
+import RPi.GPIO as GPIO
+import time, datetime
+import os
+import sys
+import telepot
+import telepot.helper
+from telepot.loop import MessageLoop
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.delegate import (
+    per_chat_id, create_open, pave_event_space, include_callback_query_chat_id)
+
+now = datetime.datetime.now()
+
+GPIO.setwarnings(False)
+
+# Inisialisasi Pin
+DIR_MTR1 = 14
+DIR_MTR2 = 15
+STEP_MTR1 = 12
+STEP_MTR2 = 13
+DRIVER = 18
+RELAY = 23
+
+# Perintah untuk menggunakan pin board GPIO Raspberry Pi
+GPIO.setmode(GPIO.BCM)
+
+# Pengaturan GPIO 
+GPIO.setup(DIR_MTR1, GPIO.OUT)
+GPIO.setup(DIR_MTR2, GPIO.OUT)
+GPIO.setup(STEP_MTR1, GPIO.OUT)
+GPIO.setup(STEP_MTR2, GPIO.OUT)
+GPIO.setup(DRIVER, GPIO.OUT)
+GPIO.setup(RELAY, GPIO.OUT)
+
+GPIO.output(RELAY, 1) #Off initially
+
+propose_records = telepot.helper.SafeDict()  # thread-safe dict
+
+class Lover(telepot.helper.ChatHandler):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[
+                   InlineKeyboardButton(text='Beri pakan', callback_data='pakan'),
+                   InlineKeyboardButton(text='Pengaturan', callback_data='setting')], [
+                   InlineKeyboardButton(text='um ...', callback_data='no'),
+                   InlineKeyboardButton(text='keluar', callback_data='keluar')
+               ]])
+    keyboard1 = InlineKeyboardMarkup(inline_keyboard=[[
+                   InlineKeyboardButton(text='Set penjadwalan pemberian pakan?', callback_data='waktu')], [
+                   InlineKeyboardButton(text='Set dosis pemberian pakan?', callback_data='dosis')
+               ]])
+    keyboard2 = InlineKeyboardMarkup(inline_keyboard=[[
+                   InlineKeyboardButton(text='Kolam 1', callback_data='kolam1'),
+                   InlineKeyboardButton(text='Kolam 2', callback_data='kolam2')], [
+                   InlineKeyboardButton(text='Kolam 3', callback_data='kolam3'),
+                   InlineKeyboardButton(text='Semua Kolam', callback_data='kolamAll')], [
+                   InlineKeyboardButton(text='Kembali', callback_data='kembali')
+               ]])
+    keyboard3 = InlineKeyboardMarkup(inline_keyboard=[[
+                   InlineKeyboardButton(text='Kolam 1', callback_data='kolam1'),
+                   InlineKeyboardButton(text='Kolam 2', callback_data='kolam2')], [
+                   InlineKeyboardButton(text='Kolam 3', callback_data='kolam3'),
+                   InlineKeyboardButton(text='Semua Kolam', callback_data='kolamAll')], [
+                   InlineKeyboardButton(text='Kembali', callback_data='kembali')
+               ]])
+    keyboard4 = InlineKeyboardMarkup(inline_keyboard=[[
+                   InlineKeyboardButton(text='Kolam 1', callback_data='kolam1'),
+                   InlineKeyboardButton(text='Kolam 2', callback_data='kolam2')], [
+                   InlineKeyboardButton(text='Kolam 3', callback_data='kolam3'),
+                   InlineKeyboardButton(text='Semua Kolam', callback_data='kolamAll')], [
+                   InlineKeyboardButton(text='Kembali', callback_data='kembali')
+               ]])
+
+    def __init__(self, *args, **kwargs):
+        super(Lover, self).__init__(*args, **kwargs)
+
+        # Retrieve from database
+        global propose_records
+        if self.id in propose_records:
+            self._edit_msg_ident = propose_records[self.id]
+            self._editor = telepot.helper.Editor(self.bot, self._edit_msg_ident) if self._edit_msg_ident else None
+        else:
+            self._edit_msg_ident = None
+            self._editor = None
+
+    def _propose(self):
+        sent = self.sender.sendMessage('Apa yang bisa saya bantu?', reply_markup=self.keyboard)
+        self._editor = telepot.helper.Editor(self.bot, sent)
+        self._edit_msg_ident = telepot.message_identifier(sent)
+    
+    def _propose1(self):
+        sent = self.sender.sendMessage('Apa yang ingin anda *atur?', reply_markup=self.keyboard1)
+        self._editor = telepot.helper.Editor(self.bot, sent)
+        self._edit_msg_ident = telepot.message_identifier(sent)
+        
+    def _propose2(self):
+        sent = self.sender.sendMessage('Kolam mana yang ingin anda beri pakan?', reply_markup=self.keyboard2)
+        self._editor = telepot.helper.Editor(self.bot, sent)
+        self._edit_msg_ident = telepot.message_identifier(sent)
+        
+    def _propose3(self):
+        sent = self.sender.sendMessage('Kolam mana yang ingin anda atur *penjadwalan pakannya?', reply_markup=self.keyboard3)
+        self._editor = telepot.helper.Editor(self.bot, sent)
+        self._edit_msg_ident = telepot.message_identifier(sent)
+    
+    def _propose4(self):
+        sent = self.sender.sendMessage('Kolam mana yang ingin anda atur *dosis pakannya?', reply_markup=self.keyboard4)
+        self._editor = telepot.helper.Editor(self.bot, sent)
+        self._edit_msg_ident = telepot.message_identifier(sent)
+
+    def _cancel_last(self):
+        if self._editor:
+            self._editor.editMessageReplyMarkup(reply_markup=None)
+            self._editor = None
+            self._edit_msg_ident = None
+
+    def on_chat_message(self, msg):
+        if msg['from']['id'] != 1137202289:
+            bot.sendMessage(chat_id, "Maaf ini adalah bot pribadi. Akses ditolak!")
+            self.close()
+        self._cancel_last()
+        self._propose()
+
+    def on_callback_query(self, msg):
+        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+
+        if query_data == 'keluar':
+            self._cancel_last()
+            self.sender.sendMessage('Terima kasih! \nBye..')
+            self.close()
+                      
+        elif query_data == 'setting':
+            self.bot.answerCallbackQuery(query_id, text='Ok. Pertanyaan berikutnya...')
+            self._cancel_last()
+            self._propose1()
+            
+        elif query_data == 'pakan':
+            self.bot.answerCallbackQuery(query_id, text='Ok. Pertanyaan berikutnya...')
+            self._cancel_last()
+            self._propose2()
+            
+        elif query_data == 'waktu':
+            self.bot.answerCallbackQuery(query_id, text='Ok. Pertanyaan berikutnya...')
+            self._cancel_last()
+            self._propose3()
+            
+        elif query_data == 'dosis':
+            self.bot.answerCallbackQuery(query_id, text='Ok. Pertanyaan berikutnya...')
+            self._cancel_last()
+            self._propose4()
+            
+        else:
+            self.bot.answerCallbackQuery(query_id, text='Ok. Tapi aku akan terus bertanya.')
+            self._cancel_last()
+            self._propose()
+
+    def on__idle(self, event):
+        self.sender.sendMessage('Saya tahu Anda mungkin perlu sedikit waktu. Aku akan selalu di sini untukmu.')
+        self._cancel_last()
+        self.close()
+
+    def on_close(self, ex):
+        # Save to database
+        global propose_records
+        propose_records[self.id] = (self._edit_msg_ident)
+
+
+TOKEN = '1202817061:AAHLFAFoftMnaOenIt59XG_IzGIp8a7yM2M'
+
+bot = telepot.DelegatorBot(TOKEN, [
+    include_callback_query_chat_id(
+        pave_event_space())(
+            per_chat_id(types=['private']), create_open, Lover, timeout=10),
+])
+MessageLoop(bot).run_as_thread()
+print('Listening ...')
+
+while 1:
+    time.sleep(10)
---
+++ cleanup.py
@@ -0,0 +1,71 @@
+import time, datetime
+import os
+import telepot
+import RPi.GPIO as GPIO
+
+from telepot.loop import MessageLoop
+
+now = datetime.datetime.now()
+
+GPIO.cleanup()
+GPIO.setwarnings(False)
+
+# Inisialisasi Pin
+DIR_MTR1 = 14
+DIR_MTR2 = 15
+STEP_MTR1 = 12
+STEP_MTR2 = 13
+DRIVER = 18
+RELAY = 23
+
+# Perintah untuk menggunakan pin board GPIO Raspberry Pi
+GPIO.setmode(GPIO.BCM)
+
+# Pengaturan GPIO 
+GPIO.setup(DIR_MTR1, GPIO.OUT)
+GPIO.setup(DIR_MTR2, GPIO.OUT)
+GPIO.setup(STEP_MTR1, GPIO.OUT)
+GPIO.setup(STEP_MTR2, GPIO.OUT)
+GPIO.setup(DRIVER, GPIO.OUT)
+GPIO.setup(RELAY, GPIO.OUT)
+
+GPIO.output(RELAY, 1) #Off initially
+
+def action(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    #verifikasi
+    if msg['from']['id'] != 1137202289:
+        bot.sendMessage(chat_id, "Maaf ini adalah bot pribadi. Akses ditolak!")
+        exit(1)
+
+    print 'Received: %s' % command
+ 
+
+    if 'on' in command:
+        message = "Turned on "
+        
+        if 'relay' in command:
+            message = message + "led"
+            GPIO.output(RELAY, 1)
+            telegram_bot.sendMessage (chat_id, message)
+
+    if 'off' in command:
+        message = "Turned off "
+        
+        if 'relay' in command:
+            message = message + "led "
+            GPIO.output(RELAY, 0)
+            telegram_bot.sendMessage (chat_id, message)
+
+ 
+
+telegram_bot = telepot.Bot('1202817061:AAHLFAFoftMnaOenIt59XG_IzGIp8a7yM2M')
+print (telegram_bot.getMe())
+MessageLoop(telegram_bot, action).run_as_thread()
+
+print 'Up and Running....'
+
+
+while 1:
+    time.sleep(10)
